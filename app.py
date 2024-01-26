b7a310e4df87724ab1a8b3aba98d1f104ad19ed6
Gabriel San Martin <gssm.snf19@uea.edu.br>
2024-01-26 16:36:03 -0400

    Using Gemini API

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import getpass
 import os
 
-import torch
-import transformers
-from langchain.chains import ConversationalRetrievalChain
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain, QAGenerationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers import AutoTokenizer, AutoModel
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 DOCUMENTS_PATH = "./docs"
+
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Provide your Google API Key: ")
 
 
 def load_documents():
@@ -30,17 +33,35 @@ def main():
     documents = load_documents()
 
     # Split documents into chunks
-    document_chunks = CharacterTextSplitter(chunk_size=50, chunk_overlap=0).split_documents(documents)
+    document_chunks = CharacterTextSplitter(separator="\n", chunk_size=300, chunk_overlap=0).split_documents(documents)
+
+    print(len(document_chunks))
 
     # Load embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="neuralmind/bert-base-portuguese-cased")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
     # Create vector store
     vector_store = Chroma.from_documents(document_chunks, embeddings, persist_directory="./chroma_db")
     vector_store.persist()
 
     # Create memory
-    # memory = ConversationBufferMemory(memory_key="bstqb_chat_memory", return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    # Use Gemini Pro as Conversational Model
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+
+    qa_model = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+
+    question = input("Please insert your question: ")
+
+    while question:
+        answer = qa_model.invoke({"question": question})["answer"]
+        print(answer)
+        question = input("Please insert your question: ")
 
 
 if __name__ == "__main__":
